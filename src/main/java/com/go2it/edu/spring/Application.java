8563e6f3df1a8fef5b531b4f4f05d8ad99938f5f
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-03-23 08:05:08 -0400

    Logback is added, build is fixed

--- src/main/java/com/go2it/edu/spring/Application.java
+++ src/main/java/com/go2it/edu/spring/Application.java
@@ -2,20 +2,16 @@ package com.go2it.edu.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-			Logger logger = LoggerFactory.getLogger(Application.class);
-			logger.trace("Hello World");
-			logger.debug("Hello World");
-			logger.info("Hello World");
-			logger.warn("Hello World");
-			logger.error("Hello World");
+		Logger logger = LoggerFactory.getLogger(Application.class);
+		logger.trace("Hello World");
+		logger.debug("Hello World");
+		logger.info("Hello World");
+		logger.warn("Hello World");
+		logger.error("Hello World");
 
 	}
 
